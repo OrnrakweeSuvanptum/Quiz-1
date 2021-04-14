1b2505a927deb1bc2673405cf9aac71d4f5940ce
User <User@LAPTOP-TA8UATAL>
2021-04-14 09:14:36 +0800

    Quiz 1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Racket y = new Racket(); 
+		y.setBrand();
+		y.setColour();
+		y.setLength();
+		y.setWeight();
+		
+		System.out.println("Brand: " + y.getBrand());
+		System.out.println("Colour: " + y.getColour());
+		System.out.println("Length: " + y.getLength() + "mm");
+		System.out.println("Weight: " + y.getWeight() + "g");
+		System.out.println();
+			
 	}
 
 }
--- src/Racket.java
+++ src/Racket.java
@@ -1,4 +1,49 @@
-
+import java.util.Scanner;
 public class Racket {
 
+		Scanner s = new Scanner(System.in);
+		
+		String brand;
+		String colour;
+		int length;
+		double weight; 
+		
+		//Create Setter Method
+		public void setBrand() {
+			System.out.print("Enter brand:");
+			this.brand = s.next(); 
+		}
+		
+		public void setColour() {
+			System.out.print("Enter colour:");
+			this.colour = s.next();
+		}
+		
+		public void setLength() {
+			System.out.print("Enter length:");
+			this.length = s.nextInt();
+		}
+		
+		public void setWeight() {
+			System.out.print("Enter weight:");
+			this.weight = s.nextDouble();
+		}
+		
+		//Create Getter Method
+		public String getBrand() {
+			return this.brand;
+		}
+		
+		public String getColour() {
+			return this.colour;
+		}
+		
+		public Integer getLength() {
+			return this.length;
+		}
+		
+		public double getWeight() {
+			return this.weight;
+		}
+		
 }
